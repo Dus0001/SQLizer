197eef37067cdb368d6997b7c234794884cf0430
Daven <davensteen@gmail.com>
2022-11-07 22:32:44 -0600

    create tables in schema.db

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employeee;
+
+CREATE TABLE departments (
+    id INTEGER Auto_INCREMENTS PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+    id INTEGER Auto_INCREMENTS PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL
+    department_id INTEGER 
+);
+
+CREATE TABLE employee (
+id INTEGER Auto_INCREMENTS PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER NOT NULL,
+);
